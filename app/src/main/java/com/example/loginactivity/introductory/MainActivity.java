5a94e35cb767abacc01fc554d01c3b73bc97b0be
Radhika Nibandhe <58497366+radhikanibandhe@users.noreply.github.com>
2021-05-19 23:18:47 +0530

    Modified Design

--- app/src/main/java/com/example/loginactivity/introductory/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/introductory/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -20,6 +21,7 @@ import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
     ImageView logo;
     LottieAnimationView lottieAnimationView;
     @Override
@@ -28,11 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logo = findViewById(R.id.logo);
+        textView = findViewById(R.id.textlogo);
         lottieAnimationView = findViewById(R.id.animation);
 
         //Adding action listner to show proper output with delay
-        logo.animate().translationY(-1700).setDuration(1000).setStartDelay(5000);
-        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(5000).setListener(new Animator.AnimatorListener() {
+        logo.animate().translationY(-1700).setDuration(1000).setStartDelay(3000);
+        textView.animate().translationY(-1700).setDuration(1000).setStartDelay(3000);
+        lottieAnimationView.animate().translationY(1600).setDuration(1000).setStartDelay(3000).setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
 
@@ -76,13 +80,13 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<PaperOnboardingPage> getDataForOnboarding() {
 
         PaperOnboardingPage src1 = new PaperOnboardingPage("Fresh Food","Get best quality food everyday",
-                Color.parseColor("#b6c9f0"),R.drawable.hotel,R.drawable.dish);
+                Color.parseColor("#F5ABC9"),R.drawable.restaurant,R.drawable.dish);
 
         PaperOnboardingPage src2 = new PaperOnboardingPage("Fast Delivery","Get fast delivery at your doorstep",
-                Color.parseColor("#77acf1"),R.drawable.scooter,R.drawable.fast);
+                Color.parseColor("#F4A9A8"),R.drawable.delivery,R.drawable.fast);
 
         PaperOnboardingPage src3 = new PaperOnboardingPage("Easy Payment","Get Payment done as fast as Cheetah",
-                Color.parseColor("#fcecdd"),R.drawable.payment,R.drawable.card);
+                Color.parseColor("#233E8B"),R.drawable.payment,R.drawable.card);
 
         ArrayList <PaperOnboardingPage> elements = new ArrayList<>();
         elements.add(src1);
